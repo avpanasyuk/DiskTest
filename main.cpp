dc98781ba92650d39127a930566ac9183786eb8e
Alexander Panasyuk <alexander.panasyuk@gmail.com>
2024-01-20 12:59:31 -0500

    Work fine, but maximum speed is slow. Will try to use async writing and do TRIM next.

--- main.cpp
+++ main.cpp
@@ -10,8 +10,10 @@ using namespace std;
 using namespace std::chrono;
 namespace fs = filesystem;
 
-static constexpr decltype(fs::space_info::available) TimeMarkArrSize = 600;
+// static constexpr decltype(fs::space_info::available) TimeMarkArrSize = 600;
 static constexpr uint8_t TimeBits = 20;
+static constexpr uint32_t FileBufferSize = 1UL<<22;
+static char FileBuffer[FileBufferSize];
 
 static uint32_t millis() {
   return (uint32_t)duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count();
@@ -22,40 +24,46 @@ int main(int argc, char* argv[]) {
   std::string Path = ".\\";
   if(argc >= 2) Path = argv[1];
 
-  std::remove((Path + "tmp.bin").c_str());
+  if(std::remove((Path + "tmp.bin").c_str())) perror((Path + "tmp.bin").c_str());
   fs::space_info tmp = fs::space(Path);
-  ofstream wf(Path + "tmp.bin", fstream::out | ofstream::binary);
+  cout << "Space available on disk is " << (tmp.available >> 20) << " MB, I will use 3/4 of it." << std::endl;
+  cout << "Speed is reported in MB/sec" << std::endl;
+
+  ofstream wf;
+  // wf.rdbuf()->pubsetbuf(FileBuffer,FileBufferSize);
+  wf.open(Path + "tmp.bin", fstream::out | ofstream::binary);
+
   if(wf.fail()) {
-    std::perror("Cannot open file for reading");
+    std::perror("Cannot open file for reading: ");
     return 1;
   }
 
-  uint32_t TimeMarkArr[TimeMarkArrSize] = {millis()};
-  uint64_t TimeMarkPeriod = avp::CeilRatio(tmp.available,TimeMarkArrSize), TimeMarkCounter = 0;
-  uint64_t V = 0;
-  decltype(fs::space_info::available) TimeMarkI = 0;
-  if(tmp.available*3/4 < 2*sizeof(TimeMarkArr) + 1) {
-    cerr << "Available space is too small!" << std::endl;
-    return 3;
-  }
+  uint32_t TimeMarkStart(millis()), TM_Prev = TimeMarkStart, TM_Cur;
+  uint32_t TimeMarkPeriod = 10000UL;
+  uint64_t V = 0, Vprev = V;
+  // if(tmp.available*3/4 < 2*sizeof(TimeMarkArr) + 1) {
+  //  cerr << "Available space is too small!" << std::endl;
+  //  return 3;
+  //}
 
   while(!wf.fail()) {
     wf.write((const char *)&V,sizeof(V));
-    if(++V >= tmp.available*3/4 - 2*sizeof(TimeMarkArr));
-    if(++TimeMarkCounter == TimeMarkPeriod) {
-      wf.flush();
-      TimeMarkCounter = 0;
-      TimeMarkArr[TimeMarkI++] = millis();
-      //  Let's print speed
-      if(TimeMarkI > 2) {
-        cout << TimeMarkPeriod*sizeof(V)/(TimeMarkArr[TimeMarkI-1] - TimeMarkArr[TimeMarkI-2])/1000 << " ";
-      }
+    if(++V >= tmp.available*3/4) break;
+    TM_Cur = millis();
+    if(TM_Cur - TM_Prev > TimeMarkPeriod) {
+      // wf.flush();
+      cout << (V - Vprev)*sizeof(V)/(TM_Cur - TM_Prev)/1000 << " ";
+      TM_Prev = TM_Cur;
+      Vprev = V;
     }
   }
 
+  if(wf.fail()) perror("Writing failed");
+  cout << "Wrote " << (V >> 20) << "MB" << std::endl;
+
   wf.close();
-  cout << std::endl << "Wrote " << V*sizeof(V)/1000000UL << " MB in " << (millis() - TimeMarkArr[0])/1000 << " s at " <<
-    V*sizeof(V)/(millis() - TimeMarkArr[0]) << " KB/s." << std::endl;
+  cout << std::endl << "Wrote " << V*sizeof(V)/1000000UL << " MB in " << (millis() - TimeMarkStart)/1000 << " s at " <<
+       V*sizeof(V)/(millis() - TimeMarkStart) << " KB/s." << std::endl;
 
   // now reading and checking
   ifstream rf(Path + "tmp.bin", fstream::in | ifstream::binary);
@@ -64,7 +72,7 @@ int main(int argc, char* argv[]) {
     return 2;
   }
   V = 0;
-  TimeMarkArr[TimeMarkI = 0] = millis();
+  TimeMarkStart = millis();
   while(!rf.eof()) {
     decltype(V) Vread;
     rf.read((char *)&Vread,sizeof(Vread));
@@ -72,19 +80,20 @@ int main(int argc, char* argv[]) {
       cerr << std::endl << "Wrong read value " << Vread << " at " << V - 1 << " position" << std::endl;
     }
 
-    if(++TimeMarkCounter == TimeMarkPeriod) {
-      TimeMarkCounter = 0;
-      TimeMarkArr[TimeMarkI++] = millis();
-      //  Let's print speed
-      if(TimeMarkI > 2) {
-        cout << TimeMarkPeriod*sizeof(V)/(TimeMarkArr[TimeMarkI-1] - TimeMarkArr[TimeMarkI-2])/1000 << " ";
-      }
+    TM_Cur = millis();
+    if(TM_Cur - TM_Prev > TimeMarkPeriod) {
+      // wf.flush();
+      cout << (V - Vprev)*sizeof(V)/(TM_Cur - TM_Prev)/1000 << " ";
+      TM_Prev = TM_Cur;
+      Vprev = V;
     }
   }
 
+  if(rf.fail()) perror("Reading failed");
+
   rf.close();
-  cout << std::endl << "Read " << V*sizeof(V)/1000000UL << " MB in " << (millis() - TimeMarkArr[0])/1000 << " s at " <<
-    V*sizeof(V)/(millis() - TimeMarkArr[0]) << " KB/s." << std::endl;
+  cout << std::endl << "Read " << V*sizeof(V)/1000000UL << " MB in " << (millis() - TimeMarkStart)/1000 << " s at " <<
+       V*sizeof(V)/(millis() - TimeMarkStart) << " KB/s." << std::endl;
   std::remove((Path + "tmp.bin").c_str());
   return 0;
 }// main
